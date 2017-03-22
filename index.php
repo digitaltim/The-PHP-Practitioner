9dab3d5cefaa57e98822f296a1364f12400c3800
Tim McHugh <tim@digitaltim.com>
2017-03-22 13:57:11 -0400

    Introduce classes
    Use local web server: php -S localhost:8888
    
    https://laracasts.com/series/php-for-beginners/episodes/12

--- index.php
+++ index.php
@@ -2,13 +2,36 @@
 
 require "functions.php";
 
-$task = [
-	'title' => 'Homework',
-	'due' => 'today',
-	'assigned_to' => 'Tim',
-	'completed' => true
+/**
+* Task class
+*/
+class Task {
+
+	public $description;
+	public $completed = false;
+
+	public function __construct($description)
+	{
+		$this->description = $description;
+	}
+
+	public function complete()
+	{
+		$this->completed = true;
+	}
+
+	protected function isComplete ()
+	{
+		return $this->completed;
+	}
+}
+
+$tasks = [
+	new Task('Go to the store'),
+	new Task('Finish my screen cast'),
+	new Task('Clean the house')
 ]; 
 
-dd($task);
-
+$tasks[0]->complete();
+//dd($tasks);
 require 'index.view.php';
