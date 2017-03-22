56e500836dca6c4d5ac41897558835a97d871a2f
Tim McHugh <tim@digitaltim.com>
2017-03-22 10:28:01 -0400

    Introduce functions
    Use local web server: php -S localhost:8888
    
    https://laracasts.com/series/php-for-beginners/episodes/10

--- index.php
+++ index.php
@@ -1,10 +1,14 @@
 <?php
 
+require "functions.php";
+
 $task = [
 	'title' => 'Homework',
 	'due' => 'today',
 	'assigned_to' => 'Tim',
 	'completed' => true
 ]; 
+
+dd($task);
 
 require 'index.view.php';
