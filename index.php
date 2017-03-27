3f6dab72ab9781f6d14d79276e7fbdc1d63ca064
Tim McHugh <tim@digitaltim.com>
2017-03-27 12:25:52 -0400

    Introduce array functions array_filter, array_map, array_column
    Use local web server: php -S localhost:8888
    
    Uses PostgreSQL database
    
    https://laracasts.com/series/php-for-beginners/episodes/16

--- index.php
+++ index.php
@@ -1,13 +1,43 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 
 require 'functions.php';
 
 require 'core/bootstrap.php';
 
-// `dd($app);
+/**
+* Test class to demonstrate array functions
+*/
+class Post
+{
+	public $title;
+	public $published;
+
+	function __construct($title, $published)
+	{
+		$this->title = $title;
+		$this->published = $published;
+	}
+}
+
+$posts = [
+	new Post('My first post', true),
+	new Post('My second post', true),
+	new Post('My third post', true),
+	new Post('My fourth post', false)
+];
+
+// $publishedPosts = array_filter($posts, function ($post) {
+// 	return $post->published;
+// });
+// dd($publishedPosts);
+
+// $modified = array_map(function ($post) {
+// 	return ['title' => $post->title];
+// }, $posts);
+// dd($modified);
+
+$titles = array_column($posts, 'title');
+dd($titles);
 
 require Router::load('routes.php')
 	->direct(Request::uri());
