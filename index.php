c76a524c000e32bf797b4ed65f042b854838ba50
Tim McHugh <tim@digitaltim.com>
2017-03-28 17:05:20 -0400

    Introduce namespaces
    Use local web server: php -S localhost:8888
    
    Uses PostgreSQL database
    
    https://laracasts.com/series/php-for-beginners/episodes/24

--- index.php
+++ index.php
@@ -5,10 +5,10 @@ ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
 require 'vendor/autoload.php';
-
 require 'functions.php';
-
 require 'core/bootstrap.php';
+
+use App\Core\{Router, Request};
 
 Router::load('routes.php')
 	->direct(Request::uri(), Request::method());
