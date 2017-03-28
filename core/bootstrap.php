01733a34e1521cdbc0ad55fd445c53e3d4da02eb
Tim McHugh <tim@digitaltim.com>
2017-03-28 10:00:00 -0400

    Introduce Composer Autoloading
    Use local web server: php -S localhost:8888
    
    Uses PostgreSQL database
    
    https://laracasts.com/series/php-for-beginners/episodes/21

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -4,9 +4,9 @@ $app = [];
 
 $app['config'] = require 'config.php';
 
-require 'core/Request.php';
-require 'core/Router.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
+// require 'core/Request.php';
+// require 'core/Router.php';
+// require 'core/database/Connection.php';
+// require 'core/database/QueryBuilder.php';
 
 $app['database'] = new QueryBuilder(Connection::make($app['config']['database']));
--- index.php
+++ index.php
@@ -4,6 +4,8 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+require 'vendor/autoload.php';
+
 require 'functions.php';
 
 require 'core/bootstrap.php';
